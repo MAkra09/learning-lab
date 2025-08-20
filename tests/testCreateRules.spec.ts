8f4610db51105e5d094e69821387c936b01d56e4
Akaracheewin Bualuang <makra@Akaracheewins-MacBook-Air.local>
2025-08-20 09:26:47 +0700

    Test create rule Fail with null piority

--- tests/testCreateRules.spec.ts
+++ tests/testCreateRules.spec.ts
@@ -1,8 +1,8 @@
 import { expect, test } from "@playwright/test";
+const baseURL = 'https://crmssp-devsales1.ttbbank.local/rule-management/rules';
 
-test('Create rule with API', async ({ request }) => {
-    const baseURL = 'https://crmssp-devsales1.ttbbank.local/rule-management/rules';
-
+test.describe('Create Rule API', () => {
+    test('Create rule with API Success', async ({ request }) => {
     const response = await request.post(`${baseURL}/create`, {
         data: {
     "statusCode": "inactive",
@@ -30,7 +30,6 @@ test('Create rule with API', async ({ request }) => {
         {
             "ruleActionType": "Assignment",
             "ruleActionLevel": "to User (by Emp ID/Name)",
-            // "ruleActionMethod":
             "ruleActionValueList": [
                 {
                     "ruleActionValue": "15151",
@@ -45,14 +44,68 @@ test('Create rule with API', async ({ request }) => {
 
     expect(response.status()).toBe(200);
 
-    // อ่าน response body
+    // For reading response body
     const body = await response.json();
     console.log('Response:', body);
 
-    // ตรวจสอบค่าภายใน response
+    // For checking values within response
     expect(body.statusCode).toBe("0");
     expect(body.statusDescription).toBe("Success");
     expect(body.data).toHaveProperty("ruleId");
     expect(body.data).toHaveProperty("ruleCode");
+
+    });
+
+    test('Create rule with API Fail Because of Null Priority', async ({ request }) => {
+    const response = await request.post(`${baseURL}/create`, {
+        data: {
+    "statusCode": "inactive",
+    "rulePriority": null,
+    "ruleName": "Test rule action bug",
+    "ruleTriggers": [
+        "Lead was created"
+    ],
+    "leadOriginations": [
+        "System Initiate",
+        "User Initiate",
+        "Customer Initiate"
+    ],
+    "ruleCriteriaList": [
+        {
+            "sequence": 0,
+            "criteria": "assignRuleModel",
+            "criteriaOperator": "equal",
+            "values": [
+                "ruleStatusActive"
+            ]
+        }
+    ],
+    "ruleActionList": [
+        {
+            "ruleActionType": "Assignment",
+            "ruleActionLevel": "to User (by Emp ID/Name)",
+            "ruleActionValueList": [
+                {
+                    "ruleActionValue": "15151",
+                    "priority": 1
+                }
+            ]
+        }
+    ],
+    "employeeId": "system"
+    }
+});
+
+    expect(response.status()).toBe(400);
+
+    // For reading response body
+    const body = await response.json();
+    console.log('Response:', body);
+
+    // For checking values within response
+    expect(body.data).toBeNull();
+    expect(body.statusCode).toBe("1");
+    expect(body.statusDescription).toBe("rulePriority must not be null");
+    });
 
 });
